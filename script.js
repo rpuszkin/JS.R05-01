d5cb1fa3648c32294da8b0e92132ee2d8d4c021a
rpuszkin <130287042+rpuszkin@users.noreply.github.com>
2024-08-16 19:08:30 +0200

    letter mistake was repaired

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 function reverseArray(arr) {
   const copyArr = [...arr];
   return copyArr.reverse();
-}
+} //przykładowy input:
 
-re; //przykładowy input:
 const result = reverseArray([1, 2, 3, 4, null]);
+console.log(result);
